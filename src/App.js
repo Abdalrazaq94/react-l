5b4f450841bc875f659fef70f4b5b5518021cebf
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-07-03 22:29:59 +0100

    uploding to gitHub

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './navbar';
+import Header from './header';
+function App() {
+  return ( <div>
+<Header />
+<Navbar />
+  
+  </div> );
+}
+
+export default App;
